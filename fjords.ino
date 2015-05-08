de8bb0c0132a7511ea85c94ac607692b1aeddf05
Joshua Miller <jshuasmiller@gmail.com>
2015-05-08 14:34:24 -0500

    adds fish and buzzer screen

---
+++ fjords.ino
@@ -0,0 +1,139 @@
+#define TRACE_DELAY 2000  // original = 2500
+#define NUM_POINTS  19
+#define X           6
+#define Y           5
+#define upPin       4     // the up button pin
+#define downPin     3     // the down button pin
+
+
+void setup()
+{
+  pinMode(upPin, INPUT);
+  pinMode(downPin, INPUT);
+
+
+  pinMode(X, OUTPUT);
+  pinMode(Y, OUTPUT);
+  TCCR0A = (1<<COM0A1 | 0<<COM0A0 | 1<<COM0B1 | 0<<COM0B0 | 1<<WGM01  | 1<<WGM00);
+  TCCR0B = (0<<FOC0A | 0<<FOC0B | 0<<WGM02 | 0<<CS02  | 0<<CS01 | 1<<CS00);
+  TIMSK0 = (0<<OCIE0B | 0<<TOIE0 | 0<<OCIE0A);
+}
+
+typedef struct point_t {
+    unsigned char x;
+    unsigned char y;
+} point_t;
+
+int draw_point(point_t point, point_t offset);
+int draw(point_t *points, int point_len, point_t offset);
+int update_fish(point_t *fish_pos);
+int is_fish_eaten(point_t whale_pos, point_t *fish_pos);
+int reset_fish(point_t *fish_pos);
+void update_game_state();
+
+const unsigned char whale_points = 18;
+point_t whale[whale_points] = {
+    {65, 0}, {60, 5}, {10, 5}, {0, 15}, {5, 30}, {10, 20},
+    {30, 25}, {65, 15}, {25, 15}, {45, 35}, {75, 45}, {100, 35},
+    {105, 20}, {100, 15}, {95, 15}, {105, 10}, {100, 5}, {75, 5},
+};
+
+const unsigned char fish_points = 8;
+point_t fish[fish_points] = {
+    {0, 10}, {10, 20}, {20, 10}, {30, 20}, {30, 0}, {20, 10}, {5, 0}, {0, 12},
+};
+
+const unsigned char MAX_XY =  255;
+point_t bounding_box[4] = {
+    {0, 0}, {0, MAX_XY}, {MAX_XY, MAX_XY}, {MAX_XY, 0}
+};
+
+#define crossed_pox_points 8
+point_t crossed_box[crossed_pox_points] = {
+    {0, 0}, {0, MAX_XY}, {MAX_XY, MAX_XY}, {MAX_XY, 0}, {0, 0}, 
+    {MAX_XY, MAX_XY}, {0, MAX_XY}, {MAX_XY, 0}
+};
+
+int draw_point(point_t point, point_t offset)
+{
+    analogWrite(X, point.x + offset.x);
+    analogWrite(Y, point.y + offset.y);
+    delayMicroseconds(TRACE_DELAY);
+    return 0;
+}
+
+int draw(point_t *points, int point_len, point_t offset)
+{
+    for (int t = 0; t < point_len; t++){
+        draw_point(points[t], offset);
+    }
+    return 0;
+}
+
+point_t whale_pos = {0, 0};
+point_t no_offset = {0, 0};
+point_t fish_1 = {MAX_XY*3/4, 100};
+unsigned char missed_fish = 0;
+
+int reset_fish(point_t *fish_pos)
+{
+    fish_pos->y = random(0, 200);
+    fish_pos->x = MAX_XY;
+}
+
+int is_fish_eaten(point_t whale_pos, point_t *fish_pos)
+{
+    unsigned int dx = whale_pos.x + 80 - fish_pos->x;
+    unsigned int dy = whale_pos.y + 40 - fish_pos->y;
+    if ((abs(dx) < 10) && (abs(dy) < 50)){
+       return 1; 
+    }
+    return 0;
+}
+
+int update_fish(point_t *fish_pos)
+{
+    unsigned char dx = random(5, 12);    
+    if (fish_pos->x <= dx){
+      reset_fish(fish_pos);
+      missed_fish++;
+      for (int t = 0; t < 200; t ++){   
+        draw(crossed_box, crossed_pox_points, no_offset);
+      }
+    }
+    fish_pos->x -= dx;
+
+    if (fish_pos->y < 0)      { fish_pos->y = 0;      }
+    if (fish_pos->y > MAX_XY) { fish_pos->y = MAX_XY; }
+    return 0;
+}
+
+void update_game_state(){
+    if ((digitalRead(upPin) == HIGH) && (whale[10].y + whale_pos.y < MAX_XY - 10)) { 
+        whale_pos.y += 10;
+    } else if ((digitalRead(downPin) == HIGH) && (whale[0].y + whale_pos.y > 10)){    
+        whale_pos.y -= 10;
+    }
+    
+    unsigned char sink = random(0, 10);
+    if (whale_pos.y > sink){
+       whale_pos.y -= sink;
+    }
+
+    if (is_fish_eaten(whale_pos, &fish_1)){
+       reset_fish(&fish_1);
+    }
+
+    update_fish(&fish_1);
+    draw(whale, whale_points, whale_pos);
+    draw(fish, fish_points, fish_1);
+    draw_point(whale_pos, no_offset);
+}
+
+void loop()
+{
+    if (missed_fish < 3){
+      update_game_state();
+    } 
+    
+}
